7c7dc14112b3da493d385e6f25523b5b617f95f8
Pratik Fuse <pratikfuse20@gmail.com>
2023-10-04 13:39:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let sum = 0;
+  let prices  = document.getElementsByClassName("price");
+	for(let i = 0;i<prices.length;i++)
+		{
+			sum+= parseInt(prices[i].innerText);
+		}
+	let tr = document.createElement("tr");
+	tr.innerHTML = `<td>total price</td><td>${sum}</td>`
+	let table = document.getElementsByTagName("table")[0];
+	table.appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
